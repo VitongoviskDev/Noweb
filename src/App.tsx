775ed8f6156ad94ef3436bf8621d36f55cf1e2fe
VitorCamposDev <v_campos_dev@outlook.com>
2025-08-08 22:24:42 -0300

    chore: added GalleryModen font

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App:React.FC = () => {
   return (
-    <div className='bg-gray-900'>App</div>
+    <div className='bg-gray-900 text-white' style={{fontFamily: 'gallery-modern'}}>DIgital Solution</div>
   )
 }
 
