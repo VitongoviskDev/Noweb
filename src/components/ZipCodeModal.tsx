2a2f633393f1cfde8ccbd6b693440a9f2ef37dfe
VitongoviskDev <v_campos_dev@outlook.com>
2025-08-11 17:02:42 -0300

    fix: build errors

--- src/components/ZipCodeModal.tsx
+++ src/components/ZipCodeModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ZipCodeInput from './ZipCodeInput';
 import { IoClose, IoSearchOutline } from 'react-icons/io5';
 import axios from 'axios';
